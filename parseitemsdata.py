1b4c8b14cda119c176fd9fd7be923b4971283fc2
letfoolsdie <letfoolsdie@gmail.com>
2015-10-01 17:46:40 +0300

    Final final for parsing item data and the resulting file itself

--- parseitemsdata.py
+++ parseitemsdata.py
@@ -25,12 +25,9 @@ def extract_data(page):
         if len(r) > 0:
             return r
     return 0
-#driver = webdriver.Chrome('D:\Data Science\Poker staking data\chromedriver.exe')
+
+
 data_pd = pd.read_csv("wtf.csv", encoding="utf8")
-#data_pd['BI_actual'] = 0
-##data_pd['Cashes'] = 0
-#data_pd = data_pd[(data_pd.status != 'On sale') & (data_pd.status != 'Sold out')]
-#data_pd = data_pd[data_pd.coef.notnull()]
 
 #toWorkWith = data_pd.loc[(data_pd['BI_actual']==0) & (data_pd['Cashes']==0)]
 driver = webdriver.Firefox()
@@ -45,18 +42,6 @@ for i in range(len(data_pd)):
         if type(info) == list:
             data_pd['BI_actual'][i] = info[2]
             data_pd['Cashes'][i] = info[3]
-
-#urls = list(data_pd.item_info)
-#driver = webdriver.Firefox()
-#driver.implicitly_wait(2)
-#    
-#info = []
-#for url in urls:
-#    driver.get(url)
-#    info.append(extract_data(driver.page_source))
-#    
-#    driver.close()
-    
 
 data_pd.to_csv("wtf.csv", encoding="utf8", index=False)
 
