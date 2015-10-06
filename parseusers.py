7a0d364dec341b2fbcd17d3afa3e0d24fff3c1f0
letfoolsdie <letfoolsdie@gmail.com>
2015-10-06 18:34:10 +0300

    Added file for parsing users and file with parsed user data

--- parseusers.py
+++ parseusers.py
@@ -6,7 +6,7 @@ Created on Fri Oct  2 15:42:25 2015
 """
 
 import pandas as pd
-import mumpy as np
+import numpy as np
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -31,6 +31,27 @@ def extract_data(page):
 #        if len(r) > 0:
 #            return r
 #    return 0
+data_pd = pd.read_csv("wtf_users1.csv", encoding="utf8")
 
-user = 'https://mttmarket.com/users/55eebec761ccb65540d2383a'
+driver = webdriver.Firefox()
+#driver.get(user)
+usersInfo = []
+for i in range(len(data_pd)):
+    if (data_pd['tournaments'][i] == 0) & (data_pd['roi'][i] == 0):
+        url = data_pd['link'][i]
+        driver.get(url)
+        info = extract_data(driver.page_source)
+        if type(info) == list:
+            data_pd['profit'][i]=float(info[0][:-1])
+            data_pd['totBI'][i]=float(info[1][:-1])
+            data_pd['totCashes'][i]=float(info[2][:-1])
+            data_pd['roi'][i]=float(info[3][:-1])
+            data_pd['roiBI'][i]=float(info[4][:-1])
+            data_pd['tournaments'][i]=int(info[5])
+            data_pd['abi'][i]=float(info[6][:-1])
+            data_pd['itm'][i]=float(info[7][:-1])
+#        usersInfo.append(info)
+    
+#    
+data_pd.to_csv('wtf_users1.csv', encoding='utf8', index=False)
 
